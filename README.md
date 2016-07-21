8133ab57a734b43a59b8d60da8457ddb38c18ab2
Chris Masters <chrismasters@gmail.com>
2016-07-21 16:20:01 +0200

    styling and readme

--- README.md
+++ README.md
@@ -1,6 +1,20 @@
 # Ember-cli-vibrant-js-shim
 
-This README outlines the details of collaborating on this Ember addon.
+Extract prominent colors from an image using [Vibrant.js](http://jariz.github.io/vibrant.js/).
+
+To include in your project install the addon as usual
+
+```js
+ember install ember-cli-vibrant-js-shim
+```
+
+You can then use it in an ES6 style
+
+```js
+import Vibrant from 'vibrant';
+```
+
+[Documentation with examples](http://chrismasters.net/ember-cli-vibrant-js-shim/)
 
 ## Installation
 
---
+++ tests/dummy/app/snippets/color-extraction.js
@@ -0,0 +1,30 @@
+import Ember from 'ember';
+import Vibrant from 'vibrant';
+
+export default Ember.Component.extend({
+  tagName: 'img',
+  attributeBindings: ['src'],
+
+  imageLoader: Ember.computed(function() { return new Image(); }),
+
+  loadImage() {
+    let img = this.get('imageLoader');
+
+    if (img) {
+      img.onload = () => {
+        Ember.run(() => {
+          var vibrant = new Vibrant(img);
+          var swatches = vibrant.swatches();
+
+          // You can now access the swatches here
+          Ember.Logger.log('swatches', swatches);
+        });
+      };
+      img.src = this.get('src');
+    }
+  },
+
+  loadImageOnInsert: Ember.on('didInsertElement', function() {
+    Ember.run.scheduleOnce('afterRender', this, this.loadImage);
+  })
+});
